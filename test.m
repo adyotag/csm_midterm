a8a4a45b76d5bc19b769c795784a35dec0ae9b64
Adyota Gupta <gupta.adyota@berkeley.edu>
2020-04-02 20:17:19 -0700

    Made into a benchmark

--- test.m
+++ test.m
@@ -1,4 +1,10 @@
-obj = Assembly("Beam_Bending_Q4_16x8_PU.txt").run();
+% Benchmark for time
 
-
-obj.trf % total reaction force
+a = Assembly("Biaxial_Q4_2x2.txt").run();
+b = Assembly("Beam_Bending_Q4_4x1_Al.txt").run();
+c = Assembly("Beam_Bending_Q8_4x1_Al.txt").run();
+d = Assembly("Beam_Bending_Q9_4x1_Al.txt").run();
+e = Assembly("Beam_Bending_Q4_8x2_Al.txt").run();
+f = Assembly("Beam_Bending_Q4_16x4_Al.txt").run();
+g = Assembly("Beam_Bending_Q4_16x4_PU.txt").run();
+h = Assembly("Beam_Bending_Q9_16x4_PU.txt").run();
